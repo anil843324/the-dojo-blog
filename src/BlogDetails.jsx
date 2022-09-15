acfdbd8318673c9e8aaae79b854cf94cdae4860e
anil843324 <anilkrsingh14200@gmail.com>
2022-09-15 10:43:14 +0530

    custome hook bug fixed

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import useFetch from './useFetch'
 
 const BlogDetails = () => {
 
-      const {id}= useParams()
-       const [data,error,isPending]=useFetch(`http://localhost:8000/blogs`)
-       console.log(error)
+         const {id}= useParams()
+       
+        const { data,isPeinding,error}=useFetch(`http://localhost:8000/blogs/${id}`)
+
+         
+     
     
   return (
+    
+
+     <div className='content'>
+
+    
     <div className='blog-details'>
       
-       {/* {isPending && <div> Loading... </div> }
-       {error && <div>{error} </div>}
+         { isPeinding && <div> Loading... </div> }
+          {error && <div>{error} </div>} 
        {
 
-          data && (
+        data && (
              <article>
                <h2>{data.title}</h2>
                <p>Written by {data.author}</p>
@@ -25,10 +34,10 @@ const BlogDetails = () => {
 
              </article>
           )
-       } */}
+       } 
  
-   
-
+       </div>
+    
     </div>
   )
 }
--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 
 const useFetch=(url)=>{
 
-    const [data,setData]=useState([]);
+    const [data,setData]=useState(null);
     const [isPeinding,setIsPending]=useState(true)
-   const [error,setError]=useState(null);
+    const [error,setError]=useState(null);
 
 
 
